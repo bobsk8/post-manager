33f0abe2fd5a9554b5c2ec3b0db856aa2f531df5
Rodrigo Prado <rodrigovianaprado@gmail.com>
2020-06-07 10:27:23 +0100

    Add post and admin routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./modules/post/post.module').then(m => m.PostModule) },
+  { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
